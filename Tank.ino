4654fdb6a1627e3146049c196c988a4d85df9355
FwoopFwoop <zacharysamiyoussef@gmail.com>
2017-12-04 13:18:42 -0500

    Modify drive forward (INCOMPLETE)
    Work-in-progress, not complete.
    
    I had to change when the program actually reads gyro values to avoid
    reading to often. This lead to a stuctural change in the drive forward
    section of the code. All other drive descisions are currently commeneted
    out for purposes of testing.

--- Tank.ino
+++ Tank.ino
@@ -17,6 +17,7 @@ const int R_Direx1 = 7;            // pin sets direction of right motor, connect
 const int R_Direx2 = 8;            // pin sets direction of left motor, connected to B-In2/Phase
 const int R_Speed = 9;            // pin sets speed of right motor, connected to B-In2/Enable
 
+
 //Motion Processing Unit
 MPU6050 mpu; //mpu object itself
 const double MPU_FAIL = -12345.6789; //arbitrary value to identify gyro failure
@@ -29,9 +30,7 @@ uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
+void dmpDataReady() {mpuInterrupt = true;}
 
 //Ultrasound
 const int sensor_0 = 10; //pin for ultrasound sensor 1
@@ -42,28 +41,32 @@ const int MIN_DISTANCE = 10; //distance in cm at which the robot should turn aro
 NewPing front_sonar(sensor_0, sensor_0, MAX_DISTANCE); //forward ultrasound sensor
 NewPing back_sonar(sensor_1, sensor_1, MAX_DISTANCE);  //reverse ultrasound sensor
 
-//Pressure Plates-TODO
+//Pressure Plates 
 const int pFront = 0;
 const int pBack = 1;
 const int pLeft = 2;
 const int pRight = 3;
 
 //Drive control variables
-bool forward;
-double velocity;
+bool forward; //Stores whether or not the robot is going forward
+double velocity; //Stores the target speed of the robot
+int lastReading; //System time in milliseconds of the last gyro reading
+bool isClear; //Stores if the robot is already moving along a straight path
+int lastUpdate; //Stores the last time the robot told the serial its status
 
 //PID coefficients for driving straight
 const double p_s = 1.0;
 const double i_s = 0.0;
-const double d_s = 1.0;
+const double d_s = 0.0;
 //PID coefficients for turns
 const double p_t = 1.0;
 const double i_t = 0.0;
-const double d_t = 1.0;
+const double d_t = 0.0;
 //PID control values
 double set, in, out;
 PID pid_s(&set, &in, &out, p_s, i_s, d_s, DIRECT);
 PID pid_t(&set, &in, &out, p_t, i_t, d_t, DIRECT);
+
 
 void driveSetup(){
   //Set all drive pins to output
@@ -74,8 +77,11 @@ void driveSetup(){
   pinMode(L_Speed, OUTPUT);
   pinMode(R_Speed, OUTPUT);
 
-  forward = true;
-  velocity = 0.7;
+  forward = true; //robot should start moving forward
+  velocity = 0.7; //base forward velocity (not maxed out so that PID has room to change values)
+  lastReading = 0; //Ensures that a gyro reading will be taken at the first opportunity
+  lastUpdate = 0; //Ensures that the user will be updated at the earliest opportunity
+  isClear = false; //Tells the robot that on startup, it needs to begin a new forward trajectory
 }
 
 void killSetup(){
@@ -109,11 +115,13 @@ void mpuSetup() {
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
+    /*
     // wait for ready
     Serial.println(F("\nSend any character to begin DMP programming and demo: "));
     while (Serial.available() && Serial.read()); // empty buffer
     while (!Serial.available());                 // wait for data
     while (Serial.available() && Serial.read()); // empty buffer again
+    */
 
     // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
@@ -160,9 +168,21 @@ void pidSetup(){
 
 void setup() {
   Serial.begin(115200);
+  Serial.println("Setting up drive");
   driveSetup();
+  Serial.println("Setting up e-stop");
   killSetup();
+  Serial.println("Setting up MPU");
   mpuSetup();
+  
+  //Delay 10 seconds for gyro to stabilize
+  
+  for(int i = 0; i<10; i++){
+    Serial.println("Robot will move in " + String(10-i) + " seconds");
+    delay(1000);
+  }
+
+  Serial.println("Starting!");
 }
 
 //Uses code provided with the MPU6050 examples
@@ -261,7 +281,7 @@ void driveDuration(double left, double right, double seconds){
 }
 
 bool isPressed(int buttonPin){
-  //TODO!!!: Make this is good
+  //TODO!!!: Make sure this is good
   analogRead(buttonPin) > 10;
 }
 
@@ -283,6 +303,8 @@ void loop() {
     //If ultrasound in the direction of movement is too close, turn around
     //Then, just drive straight
 
+    /* TODO- test and configure ultrasound, buttons, turning
+    
     //Turn at side collision
     if(isPressed(pLeft)){
       turn(!forward);
@@ -290,6 +312,7 @@ void loop() {
     if(isPressed(pRight)){
       turn(forward);
     }
+    
 
     //Change direction at fron/back interaction
     if(front_sonar.ping_cm()<MIN_DISTANCE
@@ -300,16 +323,40 @@ void loop() {
        ||isPressed(pBack)){
       forward = true;
     }
+    
+    */
 
     //Drive straight
-    set = in = readYaw();
+    if(!isClear){
+      set = readYaw();
+    }
+    
+    //Update the gyro every half second
+    int currentTime = millis();
+    if(currentTime - lastReading>500){
+      in = readYaw();
+      lastReading = currentTime;
+    }
+    
     velocity = forward? abs(velocity) : -abs(velocity);
     pid_s.Compute();
-    driveTank(velocity + out, velocity - out);    
+    double left = velocity + out;
+    double right = velocity - out;
+    
+    driveTank(left, right);    
+
+    //Update the serial with drive status every second
+    if(currentTime - lastUpdate>1000){
+      Serial.print("Driving with power ");
+      Serial.print("("+String(left)+", "+String(right)+")"); //=> "(%left, %right)"
+      Serial.println("With angle "+String(in)+" and target "+String(set));
+    }
    
   }else{
     Serial.println("EMERGENCY STOPPED: Restart Robot");
   }
+
+  /* TODO- Connect and test E-Stop
   
   if(digitalRead(kill_pin)==LOW){
     delay(kill_delay);
@@ -317,4 +364,6 @@ void loop() {
       E_STOPPED = true;
     }
   }
+
+  */
 }
